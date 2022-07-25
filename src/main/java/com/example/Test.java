fbae23646f976c3d7d8ace988f2f9ca80b2fe2c3
zhujie18 <zhujie18@meituan.com>
2022-07-25 14:55:25 +0800

    test abc1111

--- src/main/java/com/example/Test.java
+++ src/main/java/com/example/Test.java
@@ -1,8 +1,17 @@
 package com.example;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println(222);
         System.out.println(222222);
+
+        try {
+            new FileOutputStream(new File("/a.txt"));
+        }catch (Throwable e) {
+            System.out.println(111);
+        }
     }
 }
