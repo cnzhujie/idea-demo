d41d481003df7c74aea5176f2c64d7bbf0eea210
cnzhujie <jie@heimalanshi.com>
2022-08-11 21:14:44 +0800

    Update Test.java

--- src/main/java/com/example/Test.java
+++ src/main/java/com/example/Test.java
@@ -6,7 +6,8 @@ import java.io.FileOutputStream;
 public class Test {
     public static void main(String[] args) {
         System.out.println(222);
-
+        System.out.println(111111111);
+        
         try {
             new FileOutputStream(new File("/a.txt"));
         }catch (Throwable e) {
